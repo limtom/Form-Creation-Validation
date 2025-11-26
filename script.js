b5ca86d9f4b8fd0787813db96d0c0363b8c63c6a
limtom <horlartom@live.com>
2025-11-26 16:10:46 +0100

    Task-0 added

---
+++ script.js
@@ -0,0 +1,50 @@
+//Ensure all elements is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  //Get the form element
+  const form = document.getElementById("registration-form");
+
+  //Get the feedback section
+  const feedbackDiv = document.getElementById("form-feedback");
+
+  form.addEventListener("submit", (event) => {
+    //Prevent default behaviour
+    event.preventDefault();
+
+    //Get all input fields
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    //Validation setup
+    let isValid = true;
+    let messages = [];
+
+    //Username validation
+    if (username.length < 3) {
+      isValid = false;
+      messages = [...messages, "Username most be at least 3 characters"];
+    }
+
+    if (!(email.includes("@") && email.includes("."))) {
+      isValid = false;
+      messages = [...messages, "Invalid email address"];
+    }
+
+    if (password.length < 8) {
+      isValid = false;
+      messages = [...messages, "Password must be at least 8 characters"];
+    }
+
+    //Feedback
+    feedbackDiv.style.display = "block";
+
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+    } else {
+      const errMessages = messages.join("<br>");
+      feedbackDiv.innerHTML = errMessages;
+      feedbackDiv.style.color = "#dc3545";
+    }
+  });
+});
